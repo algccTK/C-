afc4ab40f36308f2d5282b4383bda1831fc5346c
algccTK <459616191@qq.com>
2016-10-14 15:12:20 +0800

    Create Program.cs

---
+++ DeleteVoiceFiles/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+
+namespace test_ConsoleApplication
+{
+    class Story
+    {
+        public static void Main(string[] args)
+        {
+            DeleteVoiceFiles.Strat();
+
+        }
+    }
+}
