4a5f526fb0c2af094fe1865c0ac7dd14418cec19
algccTK <459616191@qq.com>
2016-10-20 14:54:24 +0800

    Create Program.cs

---
+++ MultiCoreTest/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace _007_MultiCore_Practice
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Programzzz.aaaaa();
+        }
+    }
+}
