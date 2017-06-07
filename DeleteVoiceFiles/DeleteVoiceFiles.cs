40f5a6c9939c43e613e31f57a4880d538dade6b7
algccTK <459616191@qq.com>
2017-06-07 11:08:47 +0800

    Update DeleteVoiceFiles.cs

--- DeleteVoiceFiles/DeleteVoiceFiles.cs
+++ DeleteVoiceFiles/DeleteVoiceFiles.cs
@@ -18,7 +18,7 @@ namespace test_ConsoleApplication
             if (Input == "y")
             {
                 Delete(GetPath());
-                Console.WriteLine("操作完成，共删除"+ count.ToString() +"个文件");
+                Console.WriteLine("操作完成，共删除" + count.ToString() + "个文件");
                 Console.ReadLine();
 
             }
@@ -38,17 +38,16 @@ namespace test_ConsoleApplication
                 string patht = path as string;
                 TheFolder = new DirectoryInfo(patht);
             }
-            else TheFolder =path as DirectoryInfo;
-            
+            else TheFolder = path as DirectoryInfo;
+
             foreach (FileInfo item in TheFolder.GetFiles())//遍歷當前目錄下的文件
             {
-                if (item.Extension == ".ogg" || item.Extension == ".wav" )
+                if (item.Extension == ".ogg" || item.Extension == ".wav")
                 {
-                    Console.WriteLine("准备删除:"+item.FullName);
+                    Console.WriteLine("准备删除:" + item.FullName);
                     File.Delete(item.FullName);
                     count++;
-                    Console.WriteLine("删除 "+item.FullName + "完成");
-                    
+                    Console.WriteLine("删除 " + item.FullName + "完成");
                 }
             }
 
@@ -56,7 +55,27 @@ namespace test_ConsoleApplication
             {
                 Delete(item);
             }
-
+            if (TheFolder.GetDirectories().Length == 0 && TheFolder.GetFiles().Length == 0)  //空文件夾
+            {
+                Console.WriteLine("准备删除空文件夾:" + TheFolder.FullName);
+                TheFolder.Delete();
+                Console.WriteLine("删除空文件夾 " + TheFolder.FullName + "完成");
+            }
         }
+
+        private static bool IsCustomExtension(string fileExtension)
+        {
+            if (Gobal.CustomExtension == null || Gobal.CustomExtension.Length == 0) return false;
+            foreach (var item in Gobal.CustomExtension)
+            {
+                if (fileExtension == item) return true;
+            }
+            return false;
+        }
+    }
+
+    class Gobal
+    {
+        public static string[] CustomExtension { get; set; }
     }
 }
