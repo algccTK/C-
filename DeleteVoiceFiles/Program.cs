2c36ebbbebbe120b97325bbb90dfc006f50c4223
algccTK <459616191@qq.com>
2017-06-07 14:24:50 +0800

    Update Program.cs

--- DeleteVoiceFiles/Program.cs
+++ DeleteVoiceFiles/Program.cs
@@ -11,7 +11,10 @@ namespace test_ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Gobal.CustomExtension=args;
+            Console.WriteLine("輸入額外擴展名，無則直接回車");
+            var input = Console.ReadLine();
+            Gobal.CustomExtension=input.Split(' ');
+            
             DeleteVoiceFiles.Strat();
 
         }
